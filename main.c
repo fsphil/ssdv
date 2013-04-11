17766a8447976d313205427050563a23f725f255
Philip Heron <phil@sanslogic.co.uk>
2013-04-11 23:41:07 +0100

    Callsigns 6 characters long where not being null terminated

--- main.c
+++ main.c
@@ -55,7 +55,7 @@ int main(int argc, char *argv[])
 		case 'c':
 			if(strlen(optarg) > 6)
 				fprintf(stderr, "Warning: callsign is longer than 6 characters.\n");
-			strncpy(callsign, optarg, 6);
+			strncpy(callsign, optarg, 7);
 			break;
 		case 'i': image_id = atoi(optarg); break;
 		case 't': droptest = atoi(optarg); break;
